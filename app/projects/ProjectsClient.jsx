86f86e8a6156f5453fbd94176406021f66de2be8
Savi118 <sakshamviraj@amityonline.com>
2025-11-24 09:36:50 +0530

    Update ProjectsClient.jsx

--- app/projects/ProjectsClient.jsx
+++ app/projects/ProjectsClient.jsx
@@ -18,7 +18,7 @@ export default function ProjectsClient({ projects }) {
 
         {/* Grid */}
         <div className='mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-10'>
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div
               key={project._id}
               initial={{ opacity: 0, y: 40 }}
@@ -36,20 +36,24 @@ export default function ProjectsClient({ projects }) {
                 />
               </div>
 
-              {/* Overlay */}
-              <div className='absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center pointer-events-none'>
+              {/* Hover Overlay */}
+              <div
+                className='absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 
+                transition duration-300 flex items-center justify-center'
+                style={{ pointerEvents: "none" }} // Ensures links still clickable
+              >
                 <p className='text-white text-lg font-semibold px-4 text-center'>
                   {project.description}
                 </p>
               </div>
 
-              {/* Content */}
+              {/* Details */}
               <div className='p-5'>
                 <h3 className='text-xl font-bold text-green-900'>
                   {project.title}
                 </h3>
 
-                {/* Tech Tags */}
+                {/* Tech Stack */}
                 <div className='mt-3 flex flex-wrap gap-2'>
                   {project.techStack?.map((tag, i) => (
                     <span
@@ -62,21 +66,22 @@ export default function ProjectsClient({ projects }) {
                 </div>
 
                 {/* Links */}
-                <div className='mt-4 flex gap-4'>
+                <div className='mt-4 flex gap-4 pointer-events-auto'>
                   {project.github && (
                     <a
                       href={project.github}
                       target='_blank'
-                      className='text-green-700 underline'
+                      className='text-green-700 underline hover:text-green-900'
                     >
                       GitHub
                     </a>
                   )}
+
                   {project.live && (
                     <a
                       href={project.live}
                       target='_blank'
-                      className='text-green-700 underline'
+                      className='text-green-700 underline hover:text-green-900'
                     >
                       Live Demo
                     </a>
